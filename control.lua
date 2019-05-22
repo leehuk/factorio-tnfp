e83ab4f23c83b72801bf969d9e394ac3bff54b9b
leeh <lee@leeh.uk>
2019-05-22 16:05:23 +0100

    - split direction code out

--- control.lua
+++ control.lua
@@ -1,40 +1,9 @@
 local Position = require('__stdlib__/stdlib/area/position')
 local Table = require('__stdlib__/stdlib/utils/table')
 
+require('ptnlib/direction')
 require('ptnlib/state_player')
 require('ptnlib/state_train')
-
--- ptnlib_direction_iscardinal()
---   Determines if a given direction is cardinal (N/E/S/W)
-function ptnlib_direction_iscardinal(direction)
-    if direction == defines.direction.north or direction == defines.direction.east or direction == defines.direction.south or direction == defines.direction.west then
-        return true
-    end
-    
-    return false
-end
-
--- ptnlib_direction_closest()
---   Returns the closest entity from an array
-function ptnlib_direction_closest(player, entities)
-    local closest_ent = nil
-    local closest_distance = 0
-    
-    for _, ent in pairs(entities) do
-        if closest_ent then
-            distance = Position.distance(player.position, ent.position)
-            if distance < closest_distance then
-                closest_ent = ent
-                closest_distance = distance
-            end
-        else
-            closest_ent = ent
-            closest_distance = Position.distance(player.position, ent.position)
-        end
-    end
-    
-    return closest_ent
-end
 
 -- ptnlib_stop_isptn()
 --   Validates if a stop is assigned for PTN
---
+++ ptnlib/direction.lua
@@ -0,0 +1,31 @@
+-- ptnlib_direction_iscardinal()
+--   Determines if a given direction is cardinal (N/E/S/W)
+function ptnlib_direction_iscardinal(direction)
+    if direction == defines.direction.north or direction == defines.direction.east or direction == defines.direction.south or direction == defines.direction.west then
+        return true
+    end
+    
+    return false
+end
+
+-- ptnlib_direction_closest()
+--   Returns the closest entity from an array
+function ptnlib_direction_closest(player, entities)
+    local closest_ent = nil
+    local closest_distance = 0
+    
+    for _, ent in pairs(entities) do
+        if closest_ent then
+            distance = Position.distance(player.position, ent.position)
+            if distance < closest_distance then
+                closest_ent = ent
+                closest_distance = distance
+            end
+        else
+            closest_ent = ent
+            closest_distance = Position.distance(player.position, ent.position)
+        end
+    end
+    
+    return closest_ent
+end
