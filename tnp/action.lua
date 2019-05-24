1ac41501a7989acfad495bcba2e43f8c4e374850
leeh <lee@leeh.uk>
2019-05-24 23:45:52 +0100

    - handle re-dispatch when trains in manual mode

--- tnp/action.lua
+++ tnp/action.lua
@@ -34,6 +34,7 @@ end
 function tnp_action_request_create(player)
     local target = tnp_stop_find(player)
     if target then
+        -- If the trains in manual mode this wont match, catch that below.
         local train = target.get_stopped_train()
         if train then
             tnp_message(tnpdefines.loglevel.standard, player, {"tnp_train_waiting", target.backer_name})
@@ -45,7 +46,12 @@ function tnp_action_request_create(player)
             tnp_message(tnpdefines.loglevel.core, player, {"tnp_train_invalid"})
             return
         end
-        
+
+        if tnp_state_train_get(train, 'status') == tnpdefines.train.status.arrived then
+            tnp_message(tnpdefines.loglevel.standard, player, {"tnp_train_waiting", target.backer_name})
+            return
+        end
+
         tnp_action_train_dispatch(player, target, train)
     end
 end
