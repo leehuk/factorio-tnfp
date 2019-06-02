2725f1239417aff2cdedfc8d47afb29e0bdf18d0
leeh <lee@leeh.uk>
2019-06-02 14:09:24 +0100

    - using the input shortcut again now cancels the request

--- tnp/event.lua
+++ tnp/event.lua
@@ -62,14 +62,9 @@ function tnp_handle_request(event, shortcut)
             return
         end
 
-        if shortcut then
-            tnp_train_enact(train, true, nil, nil, false)
-            tnp_action_request_cancel(player, train, {"tnp_train_cancelled"})
-            return
-        end
-
-        --- !!!: TODO
-        tnp_action_request_status(player, train)
+        tnp_train_enact(train, true, nil, nil, false)
+        tnp_action_request_cancel(player, train, {"tnp_train_cancelled"})
+        return
     else
         -- We're not handling a request.
 
